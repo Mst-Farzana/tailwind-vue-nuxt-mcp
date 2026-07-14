603e55c5ac95c6eef4eed41b3d8246e00f132ef2
Farzana <farzanap1531994@gmail.com>
2026-07-14 23:49:41 +0600

    path error

--- pages/sidebar/dashboard.vue
+++ pages/sidebar/dashboard.vue
@@ -1,11 +1,11 @@
 <script setup>
-import clientsTable from '~/components/dashboard/ClientsTable.vue';
-import FinancialStatus from '~/components/dashboard/FinanceStatus.vue';
-import FinanceTable from '~/components/dashboard/FinanceTable.vue';
-import ProductList from '~/components/dashboard/ProductList.vue';
-import TrendsList from '~/components/dashboard/TrendsList.vue';
-import profile from '~/components/dashboard/profile.vue';
-import overview from '~/overview.vue';
+// import clientsTable from '~/components/dashboard/ClientsTable.vue';
+// import FinancialStatus from '~/components/dashboard/FinanceStatus.vue';
+// import FinanceTable from '~/components/dashboard/FinanceTable.vue';
+// import ProductList from '~/components/dashboard/ProductList.vue';
+// import TrendsList from '~/components/dashboard/TrendsList.vue';
+// import profile from '~/components/dashboard/profile.vue';
+// import overview from '~/overview.vue';
 
 // definePageMeta({
 //   // middleware: 'auth',
